e3cd1c830c96d8dbae6b54c8f110d8512503ba91
lavanshuai <underratediitian@gmail.com>
2026-01-22 21:36:45 +0530

    e

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,30 +1,51 @@
-import http, { IncomingMessage, ServerResponse } from "http"
 import { URLSearchParams } from "url"
 import WebSocket,{WebSocketServer} from "ws"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/backend-common"
-const server=http.createServer((req:IncomingMessage,res:ServerResponse)=>{
-  console.log("request recieved"+req.url)
-})
-const wss=new WebSocketServer({server})
-wss.on('connection',function connection(socket,request){
-  socket.on('error',console.error);
-  const url=request.url;
-  if(!url) return;
+
+interface User {
+  ws:WebSocket,
+  userId:string,
+  rooms:string[]
+}
+const users:User[]=[];
+const wss=new WebSocketServer({port:8080});
+
+function CheckUser(token:string):string | null{
+ 
+  const decoded=jwt.verify(token,JWT_SECRET)
+    if(typeof decoded=="string"){
+      return null;
+    }
+    if(!decoded || !decoded.userId){
+      return null;
+    }
+ return decoded.userId;
+}
+wss.on('connection',function connection(ws,request){
+  ws.on('error',console.error);
+   const url=request.url;
+  if(!url) return false;
+
   const queryParams=new URLSearchParams(url.split("?")[1])
   const token=queryParams.get('token') || "";
-  const decoded=jwt.verify(token,JWT_SECRET)
-    if(!decoded || !(decoded as JwtPayload).userId){
-      socket.close();
-      return;
-    }
-  socket.on('message', function message(data,isBinary){
+  const userId=CheckUser(token);
+  if(!userId){
+    ws.close();
+    return null;
+  }
+  users.push({
+    ws,
+    rooms:[],
+    userId
+
+  })
+  ws.on('message', function message(data,isBinary){
     wss.clients.forEach(function each(client){
       if(client.readyState===WebSocket.OPEN){
         client.send(data,{binary:isBinary})
       }
     })
   })
-  socket.send('ping')
+  ws.send('ping')
 })
-server.listen(8080)
